b00f177f8152d57e4fe35876a4556b5a79118807
Manuel Blazquez <mblazquezm@gmail.com>
2026-07-10 22:11:46 +0200

    fix: improve day theme contrast across sections

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "radar-fantasy-shell-v49";
+const CACHE_NAME = "radar-fantasy-shell-v50";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./data.js", "./assets/app-icon.png"];
 
 self.addEventListener("install", (event) => {
